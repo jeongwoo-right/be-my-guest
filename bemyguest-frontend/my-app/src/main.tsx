f1aa0bb7b9e133c18d70a517dce27158591bcbc6
xecond <ksw09203@gmail.com>
2025-08-21 16:26:10 +0900

    fix: CORS 설정 추가 및 충돌 해결 반영

--- bemyguest-frontend/my-app/src/main.tsx
+++ bemyguest-frontend/my-app/src/main.tsx
@@ -6,8 +6,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> 47557a2 (WIP: user fe 임시 저장)
