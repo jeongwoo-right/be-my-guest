10fec50ba0e1e38ee7f10302fa47e93684fd6f8c
jeongwookim <jeongwoocode@gmail.com>
2025-08-21 15:05:24 +0900

    feat:implement wishlist API(add, get, delete)

---
+++ bemyguest-backend/src/main/java/com/bemyguest/backend/wish/dto/ApiResponse.java
@@ -0,0 +1,10 @@
+package com.bemyguest.backend.wish.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse {
+	private String message;
+}
